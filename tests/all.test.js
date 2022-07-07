9991d688c601e9f810a11108db76cc108d39001a
Vic <74506415+victorioxd@users.noreply.github.com>
2022-07-07 12:18:34 +0200

    ✅ Should return an array

--- tests/all.test.js
+++ tests/all.test.js
@@ -3,10 +3,5 @@ const {FumoClient} = require('../src/index');
 const client = new FumoClient();
 
 test('It should return a array with fumos: ', () => {
-  expect(client.all()).resolves.toMatchObject([
-    {
-      '_id': expect.any(String),
-      'URL': expect.any(String),
-    },
-  ]);
+  expect(client.all()).resolves.any(Array);
 });

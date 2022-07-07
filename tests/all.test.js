b45a408cc5bd233787ae3f46458f9f3b075635d7
Vic <74506415+victorioxd@users.noreply.github.com>
2022-07-07 12:21:45 +0200

    🔒️ tobe array

--- tests/all.test.js
+++ tests/all.test.js
@@ -3,5 +3,5 @@ const {FumoClient} = require('../src/index');
 const client = new FumoClient();
 
 test('It should return a array with fumos: ', () => {
-  expect(client.all()).resolves.any(Array);
+  expect(client.all()).resolves.toBeInstanceOf(Array);
 });
